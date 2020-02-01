7577f26688f55039cc89f0c8af2f1994e5463c42
Svetlana Scvetkova <magic-carving@yandex.ru>
2020-02-01 21:49:46 +0500

    Updates

---
+++ precache-manifest.6acdda0c7201f308341e062affea1717.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e7b621d8bb25fb887c3e453c34a63f9b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "fae4cefb7d995b9faede",
+    "url": "/static/css/2.4e89ce74.chunk.css"
+  },
+  {
+    "revision": "97ee81fe0a8e9d276a10",
+    "url": "/static/css/main.981f7211.chunk.css"
+  },
+  {
+    "revision": "fae4cefb7d995b9faede",
+    "url": "/static/js/2.4146ffd1.chunk.js"
+  },
+  {
+    "revision": "97ee81fe0a8e9d276a10",
+    "url": "/static/js/main.a43c94e7.chunk.js"
+  },
+  {
+    "revision": "026719a6d0bb3c29ad8b",
+    "url": "/static/js/runtime-main.967b4901.js"
+  },
+  {
+    "revision": "4e1ec8403d903dc514271d7328fbdeb3",
+    "url": "/static/media/persik.4e1ec840.png"
+  }
+]);
